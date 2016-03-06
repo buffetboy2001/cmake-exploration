d00b2096954160e643195f10bbfac164d499c747
Stuart Bowman <bowman.d.stuart@gmail.com>
2016-03-05 20:44:16 -0500

    added logger to main; works fine

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,22 @@
 //
 // Created by Stuart on 3/5/2016.
 //
+#include <logger.h>
+#include <consoleappender.h>
+#include <loglevel.h>
+#include <layout.h>
+#include <ndc.h>
+#include <loggingmacros.h>
+
+using namespace log4cplus;
 
 int main(int argc, char* argv[]) {
-/* ... */
+    SharedAppenderPtr myAppender(new ConsoleAppender());
+    myAppender->setName("myAppenderName");
+    std::auto_ptr<Layout> myLayout = std::auto_ptr<Layout>(new log4cplus::TTCCLayout());
+    myAppender->setLayout( myLayout );
+    initialize(); // start logging system
+    Logger applogger = Logger::getInstance("myapp");
+    applogger.addAppender(myAppender);
+    LOG4CPLUS_INFO(applogger,"Starting logging"); // say hello
 }
